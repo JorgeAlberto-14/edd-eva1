1513ec2580f6ec07a5fd9505bd51ad58c9080cd4
JorgeAlberto-14 <LC24550103@chihuahua2.tecnm.mx>
2026-09-04 11:56:12 -0600

    Add files via upload

---
+++ EVA1_2_STACKS/src/eva1_2_stacks/EVA1_2_STACKS.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_stacks;
+
+/**
+ *
+ * @author TheGa
+ */
+public class EVA1_2_STACKS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("INCIA main");
+     A();
+        System.out.println("TERMINA main");
+    }
+    public static void A(){
+        System.out.println("INICIA A()");
+        B();
+        System.out.println("TERMINA A()");
+    }
+    public static void B(){
+        System.out.println("INICIA B()");
+        int i  = 5;
+        double d = 5.5;
+        System.out.println("TERMINA A()");
+    }
+    
+}
