e6ed26d26b54508ab49fecd18a1224fda3c8108e
JorgeAlberto-14 <LC24550103@chihuahua2.tecnm.mx>
2026-09-08 15:57:26 -0600

    Add files via upload

---
+++ EVA1_4_OBJETOS/src/eva1_4_objetos/EVA1_4_OBJETOS.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_objetos;
+
+public class EVA1_4_OBJETOS {
+
+    
+    public static void main(String[] args) {
+        Prueba prueba = new Prueba();
+        System.out.println(prueba);
+        //ELIMINAR prueba: 
+        //Terminar el programa  --> garbage collector (libera memoria automaticamente)
+        //eliminar ''directamente'' el obejto :
+        prueba = null; 
+    }
+}
+    class Prueba{
+        
+        
+    }
+    
